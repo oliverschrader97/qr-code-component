e34fbe496c82a8ef7d9c42d661bf8a0124275c0d
Oliver Schrader <olliehd1@gmail.com>
2023-03-19 04:34:18 +0200

    Add alt text to QR code image

--- src/components/qr-code/QRCode.jsx
+++ src/components/qr-code/QRCode.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 const QRCode = () => {
   return (
     <div className='qr-code'>
-      <img src={image} />
+      <img src={image} alt='QR Code' />
       <h1>Improve your front-end skills by building projects</h1>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </div>
