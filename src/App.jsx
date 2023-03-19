c9c76abb4113358e9d45de8c439ff5a036ae2c61
Oliver Schrader <olliehd1@gmail.com>
2023-03-19 04:11:58 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import QRCode from './components/qr-code/QRCode';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <QRCode />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/qr-code/QRCode.jsx
@@ -0,0 +1,17 @@
+import image from '../../assets/images/image-qr-code.png';
+
+import './qr-code.scss';
+
+import React from 'react';
+
+const QRCode = () => {
+  return (
+    <div className='qr-code'>
+      <img src={image} />
+      <h1>Improve your front-end skills by building projects</h1>
+      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+  );
+};
+
+export default QRCode;
